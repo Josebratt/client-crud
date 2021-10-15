d580dfb370886ad0dc6c8cd68c9786adaa06633b
Jose Blanco <josebratt@gmail.com>
2021-10-15 18:05:07 -0500

    Update path.routes.ts

--- src/app/path.routes.ts
+++ src/app/path.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 // components
 import { ClienteComponent } from './cliente/cliente.component';
+import { FormularioComponent } from './formulario/formulario.component';
 import { HomeComponent } from './home/home.component';
 
 export const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'cliente', component: ClienteComponent },
+    { path: 'formulario', component: FormularioComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ]
 
