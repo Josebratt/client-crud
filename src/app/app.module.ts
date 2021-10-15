9260103c97ac74da227d5375d9d8c21a9dedc5d4
Jose Blanco <josebratt@gmail.com>
2021-10-15 15:40:18 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
+// Route
+import { ROUTING } from './path.routes';
+
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ClienteComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
