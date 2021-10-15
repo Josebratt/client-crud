97dfedae889a3ebf502ad112a5bde174990374b6
Jose Blanco <josebratt@gmail.com>
2021-10-15 15:51:41 -0500

    Update cliente.component.ts

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../interface/cliente';
+import { ClienteService } from '../services/cliente.service';
 
 @Component({
   selector: 'app-cliente',
@@ -8,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteComponent implements OnInit {
 
-  constructor() { }
+  listaCliente: Cliente[] = [];
+
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
+      this.clienteService.getClientes()
+          .subscribe( data => {
+            this.listaCliente = data;
+          });
+      console.log(this.listaCliente)    
   }
 
 }
+
+
