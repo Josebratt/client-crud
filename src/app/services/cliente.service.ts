b2debe1f40115267367c787927072622293e09de
Jose Blanco <josebratt@gmail.com>
2021-10-15 18:05:10 -0500

    Update cliente.service.ts

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cliente } from '../interface/cliente';
@@ -9,10 +9,15 @@ import { Cliente } from '../interface/cliente';
 export class ClienteService {
 
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'aplication/json'})
 
   constructor( private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
      return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  create(cliente: any): Observable<Cliente>{
+      return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
   }
 }
