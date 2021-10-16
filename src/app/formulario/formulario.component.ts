7ba3b51fce13c8c88a69aeb57c5ea9e079967837
Jose Blanco <josebratt@gmail.com>
2021-10-16 15:11:16 -0500

    Update formulario.component.ts

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -11,29 +11,11 @@ import { ClienteService } from '../services/cliente.service';
 })
 export class FormularioComponent implements OnInit {
 
-  clientes: Cliente = {
-    name: '',
-    lastName: '',
-    email: '',
-    id: 0,
-    createAt: ''
-  };
 
   constructor( private clienteService: ClienteService, private router: Router) { }
 
   ngOnInit(): void {
-    console.log("Clicked!");
-    console.log(this.clientes);
   }
 
-  public create(): void{
-      // console.log("Clicked!");
-      // console.log(this.clientes);
-
-      // this.clienteService.create(this.clientes)
-      //       .subscribe(data => {
-      //         
-      //       });
-  }
 
 }
